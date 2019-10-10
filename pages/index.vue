1392e2ead4c058948f793a0bcb9b149db8d6ea39
DevilTea <ch870814@gmail.com>
2019-10-10 23:36:22 +0800

    update homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,18 @@
 <template>
-  <div>waa</div>
+  <div class="fill-parent d-flex justify-center align-center">
+    <img src="~/static/images/narwhal.png">
+  </div>
 </template>
 
 <script>
 export default {
   name: 'HomePage',
-  components: {
+  layout ({ app }) {
+    return app.store.getters.isLoggedIn ? 'default' : 'guest'
+  },
+  created () {
+    const redirectTo = this.$store.getters.isLoggedIn ? '/projects' : '/auth'
+    this.$router.replace(redirectTo)
   }
 }
 </script>
