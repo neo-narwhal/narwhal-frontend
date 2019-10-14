78d6a0f191517f90dba04139193dd177ea924ba5
DevilTea <ch870814@gmail.com>
2019-10-14 15:34:01 +0800

    add apis: getProjectById, getProjectIds, createProject

--- plugins/api/private.js
+++ plugins/api/private.js
@@ -1,4 +1,5 @@
 import Axios from 'axios'
+import qs from 'qs'
 
 export default function createPrivateAPI (token) {
   const baseURL = 'https://narwhal.ntut.club'
@@ -24,6 +25,31 @@ export default function createPrivateAPI (token) {
     async getAvailableServices () {
       const { data } = await httpClient.get('/api/services')
       return data
+    },
+
+    async createProject (projectFormData) {
+      const { data } = await httpClient.post('/api/projects', qs.stringify(projectFormData))
+      return data.projectId
+    },
+
+    async getProjectIds () {
+      const { data } = await httpClient.get('/api/projects')
+      return data
+    },
+
+    async getProjectById (projectId) {
+      const { data: p } = await httpClient.get(`/api/projects/${projectId}`)
+      const newProjectObj = {}
+      newProjectObj.id = p.id
+      newProjectObj.name = p.name
+      newProjectObj.description = p.description
+      newProjectObj.isCustom = Boolean(p.is_custom)
+      newProjectObj.imageTag = p.image_tag
+      newProjectObj.cpu = Number(p.cpu)
+      newProjectObj.memory = Number(p.memory)
+      newProjectObj.storage = Number(p.storage)
+
+      return newProjectObj
     }
   }
 }
