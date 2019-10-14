2bfd9778007e9d17175671401c79ffc88a7fb1de
DevilTea <ch870814@gmail.com>
2019-10-14 15:32:33 +0800

    update component ProjectCard

--- components/projects/list/ProjectCard.vue
+++ components/projects/list/ProjectCard.vue
@@ -1,22 +1,49 @@
 <template>
   <v-hover v-slot:default="{ hover }">
-    <v-card class="project-card" :elevation="hover ? 12 : 2" link nuxt :to="`/projects/${id}`">
+    <v-card class="project-card" :elevation="hover ? 12 : 2">
       <v-toolbar dark color="primary">
-        <v-card-title class="title" v-text="name" />
+        <div class="headline" v-text="name" />
       </v-toolbar>
-      <v-divider />
-      <v-card-text class="subtitle-1" v-text="description" />
-      <v-card-actions>
-        <v-chip
-          class="mx-2"
-          label
-          outlined
-          color="primary"
-          small
-        >
-          Node
-        </v-chip>
-      </v-card-actions>
+      <v-card-text>
+        <v-overlay v-model="hover" absolute :opacity="0.7">
+          <v-card class="pa-3" color="#111111" dark>
+            <div class="mb-3 grey--text">
+              專案存取 URL
+            </div>
+            <div style="overflow-x: auto;" v-text="`https://aaaaaa.narwhal.ntut.club/${name}`" />
+          </v-card>
+          <div class="pt-5 d-flex justify-center">
+            <v-tooltip bottom color="grey darken-4">
+              <template v-slot:activator="{ on }">
+                <v-btn icon v-on="on">
+                  <v-icon>mdi-content-copy</v-icon>
+                </v-btn>
+              </template>
+              <span>複製 URL</span>
+            </v-tooltip>
+            <v-tooltip bottom color="primary">
+              <template v-slot:activator="{ on }">
+                <v-btn icon class="mx-7" v-on="on">
+                  <v-icon>mdi-pencil</v-icon>
+                </v-btn>
+              </template>
+              <span>編輯專案</span>
+            </v-tooltip>
+            <v-tooltip bottom color="error">
+              <template v-slot:activator="{ on }">
+                <v-btn icon v-on="on">
+                  <v-icon>mdi-delete</v-icon>
+                </v-btn>
+              </template>
+              <span>刪除專案</span>
+            </v-tooltip>
+          </div>
+        </v-overlay>
+        <div class="title mb-2" v-text="`敘述：${description || '無'}`" />
+        <div class="subtitle-2 mb-2" v-text="`vCPU： ${cpu} 顆`" />
+        <div class="subtitle-2 mb-2" v-text="`RAM：${memory} MB`" />
+        <div class="subtitle-2" v-text="`儲存空間：${storage} GB`" />
+      </v-card-text>
     </v-card>
   </v-hover>
 </template>
@@ -27,18 +54,35 @@ export default {
   props: {
     name: {
       type: String,
-      required: true,
-      default: undefined
+      default: ''
     },
     id: {
       type: String,
-      required: true,
-      default: undefined
+      default: ''
     },
     description: {
       type: String,
-      required: true,
-      default: undefined
+      default: ''
+    },
+    isCustom: {
+      type: Boolean,
+      default: false
+    },
+    imageTag: {
+      type: String,
+      default: ''
+    },
+    cpu: {
+      type: Number,
+      default: 1
+    },
+    memory: {
+      type: Number,
+      default: 512
+    },
+    storage: {
+      type: Number,
+      default: 5
     }
   }
 }
