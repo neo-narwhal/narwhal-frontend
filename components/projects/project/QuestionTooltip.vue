4f10e78e1bc5746de26bfca07f14bdb3cae4a522
DevilTea <ch870814@gmail.com>
2019-10-14 15:35:04 +0800

    add component: QuestionTooltip

---
+++ components/projects/project/QuestionTooltip.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-tooltip top>
+    <template v-slot:activator="{ on }">
+      <v-icon size="20" color="primary" v-on="on">
+        mdi-help-circle-outline
+      </v-icon>
+    </template>
+    <span v-text="text" />
+  </v-tooltip>
+</template>
+
+<script>
+export default {
+  name: 'QuestionTooltip',
+  props: {
+    text: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
